5f607f0d8b0740fecbe6954c53a465546b81cd7f
KastrolKainda <143822768+KastrolKainda@users.noreply.github.com>
2023-12-08 14:41:02 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>About Us</title>
+    <style>
+        body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1px;
+}
+
+.company-description, .team {
+    width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+}
+
+.company-description {
+    margin-top: 20px;
+}
+
+h2 {
+    text-align: center;
+    color: #333;
+}
+
+.team-member {
+    display: inline-block;
+    text-align: center;
+    margin: 10px;
+    
+}
+
+.team-member img {
+    width: 70px;
+    height: 70px;
+    border-radius: 33px;
+   
+}
+
+.team-member p {
+    margin: 5px 0;
+}
+p{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    text-align: center;
+}
+
+.team{
+    background: rgb(8, 37, 54);   
+    color: #fff;
+}
+html{
+    /*background: #222;*/
+    background: #abb0b0c9;
+}
+h1{
+    text-align: center;
+}
+
+    </style>
+</head>
+<body>
+    <header>
+        <h1>About Our Company</h1>
+    </header>
+
+    <section class="company-description">
+        <h2>Company Description</h2>
+        <p>
+        Customer satisfaction is the cornerstone of our business. We prioritize listening to our clients, understanding their 
+        <br>unique requirements, and delivering tailored solutions that consistently exceed expectations. 
+        <br>Our dedicated team of professionals is always ready to provide <br>expert guidance and support, ensuring our customers achieve their goals.
+        <br>Adaptability is in our DNA. We remain agile, evolving alongside the fast-paced digital world to keep our customers at the forefront. 
+        <br>As your trusted partner, we're here to help you navigate the complexities of the online landscape and achieve success. 
+        <br>Join us in shaping the future and experience the transformative difference 
+        <br>can make in your online journey.
+        <br><br>Thank You!
+        </p>
+ 
+    </section>
+
+    <section class="company-description">
+        <h2>Our Mission</h2>
+        <p>
+        Our mission is simple yet profound - to empower our customers and partners
+        <br>with the tools and solutions they need to thrive in a dynamic, competitive world. We believe in making a  
+        <br>positive impact on businesses and individuals alike, and this belief drives 
+        <br>every aspect of our operations.
+        </p>
+    </section>
+
+    <section class="company-description">
+        <h2>Our Vision</h2>
+        <p>
+        Our vision is simple yet profound - to empower our customers and partners
+        <br>with the tools and solutions they need to thrive in a dynamic, competitive world. We believe in making a  
+        <br>positive impact on businesses and individuals alike, and this belief drives 
+        <br>every aspect of our operations.
+        </p>
+    </section>
+
+                <h1>Our Team </h1>
+    <section class="team">
+        <div class="team-member">
+        <img src="images/abraham.jpg" alt="Team Member 2">
+            <p>A. Kalunga</p>
+            <h4>C E O</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/michelle.jpg" alt="Team Member 2">
+            <p>M. Chanda</p>
+            <h4>Designer</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/shimi.jpg" alt="Team Member 2">
+            <p>S. Jackroda</p>
+            <h4>Information Analyst</h4>
+        </div>
+        <div class="team-member">
+            <img src="images/btp.jpg" alt="Team Member 2">
+            <p>B.M Mwansa</p>
+            <h4>Chief Designer</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/elijah.jpg" alt="Team Member 2">
+            <p>M.C Mwaba</p>
+            <h4>Software Developer</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/cas.jpg" alt="Team Member 2">
+            <p>C. Kainda</p>
+            <h4>Software Developer</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/seven.jpg" alt="Team Member 2">
+            <p>C. Mwape</p>
+            <h4>Cybersecurity Agent</h4>
+        </div>
+        <div class="team-member">
+        <img src="images/sav.jpg" alt="Team Member 2">
+            <p>S. Chitoshi</p>
+            <h4>Designer</h4>
+        </div>
+          
+        <!-- <img src="team_member2.jpg" alt="Team Member 2">-->
+    </section>
+
+    <footer>
+        <br>
+        <p>&copy; 2023 Online Calculator</p>
+    </footer>
+</body>
+</html>
